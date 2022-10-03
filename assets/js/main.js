b341347777de1da6f1858b06df12eeed778f8e55
parimal kumar roy <parimal.tbd@gmail.com>
2022-10-04 00:12:14 +0600

    price section complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,22 @@
 $(document).ready(function () {
+  // mignififc popup
   $(".test-popup-link").magnificPopup({
     type: "image",
     gallery: {
       // options for gallery
       enabled: true,
     },
+  });
+
+  // counterup js
+  $(".counter").counterUp();
+
+  // slick slider
+  $(".testimonial").slick({
+    arrows: false,
+    dots: true,
+    Infinity: true,
+    loop: true,
+    autoplay: true,
   });
 });
